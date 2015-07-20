4480f0ccf7ca80fd072c3f579b6bbca7988ae770
Ryan Burke <ryaneburke@gmail.com>
2015-07-20 09:56:22 -0400

    hopefully final fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,10 +66,10 @@ class UsersController < ApplicationController
 	def photos
 	#repull current_user object out of DB
 		current_user
-		# if current_user.photos != nil
-		# 	@array_img_links = current_user.photos.pluck(:img_url)
-		# 	render :photos
-		# else
+		if current_user.photos != nil
+			@array_img_links = current_user.photos.pluck(:img_url)
+			render :photos
+		else
 	#API call to get profile photos
 			headers = {
 				:Authorization => "OAuth #{session[:access_token]}"
@@ -81,7 +81,7 @@ class UsersController < ApplicationController
 			create_and_save_photo_entries(@array_img)
 			@array_img_links = current_user.photos.pluck(:img_url)
 			render :photos
-		# end
+		end
 	end
 ##########################
 #########################	
